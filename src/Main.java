8b18658de22bcf2a00352fae32ef652f8f48f2b2
admin.ian.swerts <iansw.stank@gmail.com>
2025-01-10 12:29:41 +0000

    Pequenas correções

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,7 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-ReadLines readLines = new ReadLines();
-readLines.readARandomLine();
-Game game = new Game();
-game.startGame();
+        Game game = new Game();
+        game.startGame();
     }
 }
